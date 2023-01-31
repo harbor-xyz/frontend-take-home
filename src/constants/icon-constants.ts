2ccba6d7ca6f6b9061f3c8be7710868ebefe186d
Abhishek Wadhawan <abhishek.wadhawan@ymail.com>
2023-01-31 17:30:18 +0530

    adds sass, fonts, icons

---
+++ src/constants/icon-constants.ts
@@ -0,0 +1,87 @@
+import HarborLogo from "../assets/icons/Harbor logo.svg";
+import AlchemyLogo from "../assets/icons/Alchemy.png";
+import ArbitrumLogo from "../assets/icons/Arbitrum.png";
+import EthreumLogo from "../assets/icons/Ethereum.png";
+import FantomLogo from "../assets/icons/Fantom.png";
+import OptimismLogo from "../assets/icons/Optimism.png";
+import PolygonLogo from "../assets/icons/Polygon.png";
+import { ReactComponent as AddIcon } from "../assets/icons/Icons - Add.svg";
+import { ReactComponent as AllProjectIcon } from "../assets/icons/Icons - All Projects.svg";
+import { ReactComponent as AllStatusIcon } from "../assets/icons/Icons - All Status.svg";
+import { ReactComponent as ArrowLeftIcon } from "../assets/icons/Icons - Arrow-Left.svg";
+import { ReactComponent as ArrowRightIcon } from "../assets/icons/Icons - Arrow-Right.svg";
+import { ReactComponent as ArrowUpIcon } from "../assets/icons/Icons - Arrow-Up.svg";
+import { ReactComponent as AttentionIcon } from "../assets/icons/Icons - Attention.svg";
+import { ReactComponent as ClokeStrokeIcon } from "../assets/icons/Icons - Clock stroke.svg";
+import { ReactComponent as CloneIcon } from "../assets/icons/Icons - Clone.svg";
+import { ReactComponent as CloseCircleIcon } from "../assets/icons/Icons - Close circle.svg";
+import { ReactComponent as CloseThickIcon } from "../assets/icons/Icons - Close-Thick.svg";
+import { ReactComponent as CommandSheetIcon } from "../assets/icons/Icons - Command sheet.svg";
+import { ReactComponent as DocsIcon } from "../assets/icons/Icons - Docs.svg";
+import { ReactComponent as DownloadIcon } from "../assets/icons/Icons - Download.svg";
+import { ReactComponent as EditIcon } from "../assets/icons/Icons - Edit.svg";
+import { ReactComponent as ExternalLinkIcon } from "../assets/icons/Icons - External link.svg";
+import { ReactComponent as InfoIcon } from "../assets/icons/Icons - Information.svg";
+import { ReactComponent as KillIcon } from "../assets/icons/Icons - Kill.svg";
+import { ReactComponent as KilledIcon } from "../assets/icons/Icons - Killed.svg";
+import { ReactComponent as LeftArrowIcon } from "../assets/icons/Icons - Left arrow.svg";
+import { ReactComponent as MembersIcon } from "../assets/icons/Icons - Members.svg";
+import { ReactComponent as ProjectKeyIcon } from "../assets/icons/Icons - Project key.svg";
+import { ReactComponent as ProjectIcon } from "../assets/icons/Icons - Project.svg";
+import { ReactComponent as SettingsIcon } from "../assets/icons/Icons - Settings.svg";
+import { ReactComponent as StandingUpIcon } from "../assets/icons/Icons - Standing up.svg";
+import { ReactComponent as StartTestNetIcon } from "../assets/icons/Icons - Start testnet.svg";
+import { ReactComponent as TestNetsIcon } from "../assets/icons/Icons - Testnets.svg";
+import { ReactComponent as TextCursorIcon } from "../assets/icons/Icons - Text cursor.svg";
+import { ReactComponent as TickInCircleIcon } from "../assets/icons/Icons - Tick-in-circle.svg";
+import { ReactComponent as TickThickIcon } from "../assets/icons/Icons - Tick-Thick.svg";
+import { ReactComponent as TickThinIcon } from "../assets/icons/Icons - Tick-Thin.svg";
+import { ReactComponent as TimeIcon } from "../assets/icons/Icons - Time.svg";
+import { ReactComponent as UserKeyIcon } from "../assets/icons/Icons - User key.svg";
+import { ReactComponent as DuplicateIcon } from "../assets/icons/Duplicate.svg";
+import { ReactComponent as FailedIcon } from "../assets/icons/Failed.svg";
+
+export const ICONS = {
+  HarborLogo,
+  AlchemyLogo,
+  ArbitrumLogo,
+  EthreumLogo,
+  FantomLogo,
+  AddIcon,
+  AllProjectIcon,
+  AllStatusIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  ArrowUpIcon,
+  AttentionIcon,
+  ClokeStrokeIcon,
+  CloneIcon,
+  CloseCircleIcon,
+  CloseThickIcon,
+  CommandSheetIcon,
+  DocsIcon,
+  DownloadIcon,
+  EditIcon,
+  ExternalLinkIcon,
+  InfoIcon,
+  KillIcon,
+  KilledIcon,
+  LeftArrowIcon,
+  MembersIcon,
+  ProjectKeyIcon,
+  ProjectIcon,
+  SettingsIcon,
+  StandingUpIcon,
+  StartTestNetIcon,
+  TestNetsIcon,
+  TextCursorIcon,
+  TickInCircleIcon,
+  TickThickIcon,
+  TickThinIcon,
+  TimeIcon,
+  UserKeyIcon,
+  OptimismLogo,
+  PolygonLogo,
+  DuplicateIcon,
+  FailedIcon,
+};
