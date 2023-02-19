13c1bc150b432f7d4e54b1039ef09a1c671a84b4
Vinayak Mote <vmote@slack-corp.com>
2023-02-19 16:13:46 +0530

    Added testnet view component

--- user-interface/src/components/main-view/main-view.js
+++ user-interface/src/components/main-view/main-view.js
@@ -1,8 +1,10 @@
+import TestnetView from '../testnet-view/testnet-view';
+
 import './main-view.scss';
 
 /* Depending on the current url (as per user selection) we will render TestnetView, MemberView or ProjectKey view */
 export default function MainView() {
     return <section className="mainview">
-        <h1>Main View of the app</h1>
+        <TestnetView />
     </section>
 }
---
+++ user-interface/src/components/testnet-view/testnet-view.js
@@ -0,0 +1,6 @@
+
+export default function TestnetView() {
+    return <div>
+        Testnet View
+    </div>
+}
