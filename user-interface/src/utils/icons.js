cab0e3ea7f84f3d9ddf287140f82a8cb80b071b8
Vinayak Mote <vmote@slack-corp.com>
2023-02-19 14:19:39 +0530

    Added all icons to use them in components

---
+++ user-interface/src/utils/icons.js
@@ -0,0 +1,43 @@
+import Plus from './assets/images/Plus.png';
+import Copy from './assets/images/Copy.png';
+import Testnet from './assets/images/Testnet.png';
+import Members from './assets/images/Members.png';
+import ProjectKey from './assets/images/ProjectKey.png';
+import Star from './assets/images/Star.svg';
+import LeftArrow from './assets/images/LeftArrow.svg';
+import Dot from './assets/images/Dot.svg';
+import Hourglass from './assets/images/Hourglass.svg';
+import Settings from './assets/images/Settings.svg';
+import Timer from './assets/images/Timer.svg';
+import DownArrow from './assets/images/DownArrow.svg';
+import Ether from './assets/images/Ether.svg';
+import Polygon from './assets/images/Polygon.svg';
+import Avalance from './assets/images/Avalance.svg';
+import Fantom from './assets/images/Fantom.svg';
+import Running from './assets/images/Running.svg';
+import Failed from './assets/images/Failed.svg';
+import Killed from './assets/images/Killed.svg';
+import All from './assets/images/All.svg';
+
+export const Icons = {
+    Plus,
+    Copy,
+    Testnet,
+    Members,
+    ProjectKey,
+    Star,
+    LeftArrow,
+    Dot,
+    Hourglass,
+    Settings,
+    Timer,
+    DownArrow,
+    Ether,
+    Polygon,
+    Avalance,
+    Fantom,
+    Running,
+    Failed,
+    Killed,
+    All,
+}
