0830474887086a1134ae39ad2d98bce33f391962
Vinayak Mote <vmote@slack-corp.com>
2023-02-21 15:53:44 +0530

    Add error boundaries so that error in one component doesn't break the whole app

--- user-interface/src/components/main-view/main-view.js
+++ user-interface/src/components/main-view/main-view.js
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TestnetView from '../testnet-view/testnet-view';
 import { TESTNET_PROPTYPE } from '../../utils/types';
+import { ErrorBoundary } from 'react-error-boundary'
+import ErrorHandler from '../error-handler/error-handler';
 
 import './main-view.scss';
 
@@ -13,7 +15,9 @@ Renders different views based on the current URL selected by the user.
 */export default function MainView({ testnets }) {
     return <section className="mainview">
         <Routes>
-            <Route exact path="/" element={<TestnetView testnets={testnets} />} />
+            <Route exact path="/" element={<ErrorBoundary FallbackComponent={ErrorHandler}>
+                <TestnetView testnets={testnets} />
+            </ErrorBoundary>} />
             {/* Add your future route views here for Members and Project Key view */}
         </Routes>
     </section>
